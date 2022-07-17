f5bb3d5488df9be2b2b0a1f306bb2b97c91a629d
Ajinkya016 <77497555+Ajinkya016@users.noreply.github.com>
2022-07-17 11:53:33 +0530

    uploading files

---
+++ js/questions.js
@@ -0,0 +1,162 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Tissue that attaches the ovule to the wall of ovary.",
+    answer: "placenta",
+    options: [
+      "placentation",
+      "placenta",
+      "ovule-ovary wall",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Is corrola(petals) an essential whorl?",
+    answer: "False",
+    options: [
+      "True",
+      "False",
+      "Sometimes yes and sometimes no",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Part of flower carrying photosynthesis",
+    answer: "sepals",
+    options: [
+      "coloured tepals",
+      "petals",
+      "sepals",
+      "No part can carry photosynthesis 🙄"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Female gamets of flower are present in?",
+    answer: "Ovary",
+    options: [
+      "Ovule",
+      "Ovary",
+      "Style",
+      "Thalamus"
+    ]
+  },
+    {
+    numb: 5,
+    question: "the prefix \"Poly\" is used when the floral whorls:",
+    answer: "are free",
+    options: [
+      "are free",
+      "are fused",
+      "are more than one in a flower",
+      "are absent"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Whem staminate and pistilate flowers grow on same plant, the plant is called as",
+    answer: "monocious",
+    options: [
+      "monoadelphous",
+      "polyadelphous",
+      "monocious",
+      "dioecious"
+    ]
+  },
+    {
+    numb: 7,
+    question: "In a bilobed anther, how many pollen sacs are present in each lobe?",
+    answer: "two pollen sacs",
+    options: [
+      "four pollen sacs",
+      "two pollen sacs",
+      "no pollen sacs",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 8,
+    question: "In androecium,if stamens are free, they are called as?",
+    answer: "polyandrous",
+    options: [
+      "Monoadelphous",
+      "Diadelphous",
+      "polyadelphous",
+      "polyandrous"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Name of the part located as <b><i>A</i><b> in the diagram? <br/> <img style='height:200px;' src='Flower.png'/>",
+    answer: "receptacle",
+    options: [
+      "Sepals",
+      "epicalyx",
+      "receptacle",
+      "pedicel"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Give biological name for bisexual flower",
+    answer: "hermaphrodite flower",
+    options: [
+      "pistilate flower",
+      "Staminate flower",
+      "hermaphrodite flower",
+      "Complete or perfect flower"
+    ]
+  },
+    {
+    numb: 11,
+    question: "Name of the part located as <b><i>x</i><b> in the diagram? <br/> <img style='height:200px;' src='pollensac.png'/>",
+    answer: "pollen sac",
+    options: [
+      "pollen sac",
+      "pollen grains",
+      "fillament",
+      "Androecium"
+    ]
+  },
+    {
+    numb: 12,
+    question: "Pea is an example of:",
+    answer: "diadelphous",
+    options: [
+      "monoadelphous",
+      "diadelphous",
+      "polyadelphous",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 13,
+    question: "Second series of sepals are called as",
+    answer: "epicalyx",
+    options: [
+      "subcalyx",
+      "epicalyx",
+      "dicalys",
+      "epidisepal"
+    ]
+  },
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
